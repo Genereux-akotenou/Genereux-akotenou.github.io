4f39551fcd16fcabdebb2873c510944a6374fa98
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2024-09-10 00:40:28 +0100

    delete all category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,15 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Projects',
-	'Category 2',
+	// 'Category 1',
+	// 'Category 2',
 	// 'Category 3',
 	// 'Category 4',
 	// 'Category 5'
+	'AI & Machine Learning',
+    'Data Science & Analytics',
+    'Deep Learning Research',
+    'Applied AI in Industry',
+    'Data Visualization & Storytelling',
+    'Natural Language Processing',
+    'AI Ethics & Responsibility'
 ] as const
