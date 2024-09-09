6b8a76fb12943d63d7cd142a47381a91e15185c6
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2024-09-10 00:32:31 +0100

    customize blog

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,9 +11,9 @@ interface SiteConfig {
 export const siteConfig: SiteConfig = {
 	author: 'Généreux Akotenou', // Site author
 	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	description: 'Welcome to Généreux Akotenou\'s blog.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post -', // Message to share a post on social media
+	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
