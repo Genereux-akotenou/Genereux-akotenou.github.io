fbcd95580b5c8554aac3da8569f124fc69943f42
Genereux-akotenou <Genereux-akotenou@users.noreply.github.com>
2024-09-10 11:41:29 +0000

    deploy: f1015f621563911c4c2f6f98eb128359666767f2

--- _astro/hoisted.wp25rvV4.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./hoisted.v9oudrN4.js";import"./hoisted.lF-jLx9G.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
