cef849205acbf109e50ec2f81ac850c698561236
Genereux-akotenou <Genereux-akotenou@users.noreply.github.com>
2024-11-06 12:58:39 +0000

    deploy: 00d3b596ac3c682305422050621f1705b0c18899

--- _astro/hoisted._11KdqwE.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./hoisted.c8akvpyQ.js";import"./hoisted.vgEPtz6a.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
